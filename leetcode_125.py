8ffc4185631a72de5752d1a55f72503a65f45528
Chuncheonian <ehddud2468@gmail.com>
2021-03-19 00:03:02 +0900

    leetcode 125 - 유효한 팰린드롬 : 문자열 처리

---
+++ leetcode_125.py
@@ -0,0 +1,14 @@
+# 2021.03.18
+# Dongyoung Kwon (ehddud2468@gmail.com)
+# https://leetcode.com/problems/valid-palindrome/
+
+def isPalindrome(s: str) -> bool:
+    strs = []
+
+    for char in s:
+        if char.isalnum(): # 영문자, 숫자 여부를 판별하는 Method
+            strs.append(char.lower()) # 소문자로 변경
+  
+    return strs == strs[::-1] # 팰린드롬 여부 판별
+
+print(isPalindrome("A man a plan, a canal: Panama"))
