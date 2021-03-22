e74f778465b1d7c897a190c5aa2c47503d3d52a2
Chuncheonian <ehddud2468@gmail.com>
2021-03-22 14:26:43 +0900

    leetcode 5 - 가장 긴 팰린드롬 부분 문자열 : 투 포인터

---
+++ leetcode_5.py
@@ -0,0 +1,19 @@
+# 2021.03.22
+# Dongyoung Kwon (ehddud2468@gmail.com)
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+def longestPalindrome(s: str) -> str:   
+    # 예외 처리
+    if len(s) < 2 or s == s[::-1]: # s == s[::-1]를 빼면 문자열 전체가 팰린드롬일 경우 탐색속도가 느려짐.
+        return s
+
+    left, length = 0, 0
+    
+    for right in range(len(s)):
+        if s[right-length: right+1] == s[right-length : right+1][::-1]:
+            left, length = right-length, length+1
+        elif right-length > 0 and s[right-length-1 : right+1] == s[right-length-1 : right+1][::-1]:
+            left, length = right-length-1, length+2
+    return s[left : left+length]
+
+# print(longestPalindrome('babad'))
