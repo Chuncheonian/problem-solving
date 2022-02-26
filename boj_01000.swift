4e1351b01bff9c10ec7e7eaadd046e436fe14cb9
chuncheonian <ehddud2468@gmail.com>
2022-02-26 20:08:47 +0900

    boj 1000 - A+B : 수학 구현

---
+++ boj_01000.swift
@@ -0,0 +1,5 @@
+// 2022.02.26
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/1000
+
+print((readLine()?.split(separator: " ").map { Int($0)! }.reduce(0, +))!)
