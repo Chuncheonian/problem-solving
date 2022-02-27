6e234dd5d969628af24680d7ab582e8f3f8f4212
chuncheonian <ehddud2468@gmail.com>
2022-02-27 17:14:57 +0900

    boj 10998 - A×B : 수학

---
+++ boj_10998.swift
@@ -0,0 +1,5 @@
+// 2022.02.27
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/10998
+
+print((readLine()?.split(separator: " ").map{ Int($0)! }.reduce(1, *))!)
