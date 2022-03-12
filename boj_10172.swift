f8d081f1ae656afe2fa1a02f221f28ff7d4cf90a
chuncheonian <ehddud2468@gmail.com>
2022-03-12 15:54:06 +0900

    boj 10172 - 개 : 구현

---
+++ boj_10172.swift
@@ -0,0 +1,11 @@
+// 2022.03.12
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/10172
+
+print(#"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#)
