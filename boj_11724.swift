6429413c77295cd764245f725fce1ae987cce8b7
chuncheonian <ehddud2468@gmail.com>
2022-03-18 19:51:39 +0900

    boj 11724 - 연결 요소의 개수 : dfs

--- boj_11724.swift
+++ boj_11724.swift
@@ -1,43 +1,41 @@
-// 2022.03.15
+// 2022.03.18
 // Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
 // https://www.acmicpc.net/problem/11724
 
-let nm = readLine()!.split(separator: " ").map{ Int(String($0))! }
-let vertexCount = nm[0]
-let edgeCount = nm[1]
+import Foundation
 
-var adjacencyList = [[Int]](repeating: [], count: vertexCount+1)
+var line = readLine()!.split(separator: " ").map( { Int($0)! })
+let (n, m) = (line[0], line[1])
+var adjacencyList: [[Int]] = Array(repeating: [], count: 1001)
 
-for _ in 1...edgeCount {
-  let edgeInfo = readLine()!.split(separator: " ").map{ Int(String($0))! }
-  adjacencyList[edgeInfo[0]].append(edgeInfo[1])
-  adjacencyList[edgeInfo[1]].append(edgeInfo[0])
+for _ in 0..<m {
+    line = readLine()!.split(separator: " ").map( { Int($0)! })
+    let (v1, v2) = (line[0], line[1])
+    
+    adjacencyList[v1].append(v2)
+    adjacencyList[v2].append(v1)
 }
 
-var visited = [Int](repeating: 0, count: vertexCount + 1)
+var visited: [Bool] = Array(repeating: false, count: 1001)
 
-func bfs(_ start: Int) {
-  var queue = [start]
-  visited[start] = 1
-
-  while queue.isEmpty == false {
-    let vertex = queue.removeFirst()
-
-    for next in adjacencyList[vertex] {
-      if visited[next] == 0 {
-        visited[next] = 1
-        queue.append(next)
-      }
+func dfs(start: Int) {
+    visited[start] = true
+    
+    for i in 0..<adjacencyList[start].count {
+        let next = adjacencyList[start][i]
+        
+        if visited[next] == false {
+            dfs(start: next)
+        }
     }
-  }
 }
 
 var count = 0
-for i in 1...vertexCount {
-  if visited[i] == 0 {
-    bfs(i)
-    count += 1
-  }
+for i in 1...n {
+    if visited[i] == false {
+        dfs(start: i)
+        count += 1
+    }
 }
 
 print(count)
