c84bcc2e094524b5a9b14c8d6cf34d5a775225b2
chuncheonian <ehddud2468@gmail.com>
2022-04-04 17:32:06 +0900

    boj 15596 - 정수 N개의 합 : 구현

---
+++ boj_15596.py
@@ -0,0 +1,9 @@
+# 2022.04.04
+# Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+# https://www.acmicpc.net/problem/15596
+
+def solve(a: list) -> int:
+    return sum(a)
+
+# Testcase
+# print(solve([1, 2, 3]))  # > 6
