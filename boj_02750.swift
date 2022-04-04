71f923b51840761584eb3b475e7ca76ddc9b4c6d
chuncheonian <ehddud2468@gmail.com>
2022-04-04 16:27:15 +0900

    boj 2750 - 수 정렬하기 : 정렬

---
+++ boj_02750.swift
@@ -0,0 +1,11 @@
+// 2022.04.04
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/2750
+
+var arr = [Int]()
+
+for _ in 1...Int(readLine()!)! {
+    arr.append(Int(readLine()!)!)
+}
+
+arr.sorted().forEach{ print($0) }
