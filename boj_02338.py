ffb5e295ac40b047f223276d165ec00e8c559965
chuncheonian <ehddud2468@gmail.com>
2022-04-11 01:05:58 +0900

    boj 2338 - 긴자리 계산 : 수학

---
+++ boj_02338.py
@@ -0,0 +1,10 @@
+# 2022.04.11
+# Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+# https://www.acmicpc.net/problem/2338
+
+A = int(input())
+B = int(input())
+
+print(A+B)
+print(A-B)
+print(A*B)
