e45526115bb42b2d2d64fec71118aac37144a4dd
chuncheonian <ehddud2468@gmail.com>
2022-04-16 01:35:11 +0900

    boj 5338 - 마이크로소프트 로고 : 구현

---
+++ boj_05338.swift
@@ -0,0 +1,11 @@
+// 2022.04.16
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/5338
+
+print(#"""
+       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|
+"""#)
