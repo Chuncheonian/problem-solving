97ce1adfc46fff4f9ce0b495d3cd7f7d7db011cb
chuncheonian <ehddud2468@gmail.com>
2022-04-19 12:52:42 +0900

    boj 11866 - 요세푸스 문제 0 : 큐

---
+++ boj_11866.swift
@@ -0,0 +1,19 @@
+// 2022.04.19
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/11866
+
+let nk = readLine()!.split{ $0 == " " }.map{ Int(String($0))! }
+let (n, k) = (nk[0], nk[1])
+
+var queue = [Int](1...n)
+var result = [Int]()
+result.reserveCapacity(n)
+var cnt: Int = k - 1
+
+while true {
+    result.append(queue.remove(at: cnt))
+    if queue.isEmpty { break }
+    cnt = (cnt + k - 1) % queue.count
+}
+
+print("<"+result.map{ String($0) }.joined(separator: ", ")+">")
