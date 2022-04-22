58ec6d46f53cfcac3525534ffef3b879be488308
chuncheonian <ehddud2468@gmail.com>
2022-04-23 02:21:16 +0900

    boj 5339 - 콜센터 : 구현

---
+++ boj_05339.swift
@@ -0,0 +1,16 @@
+// 2022.04.23
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/5339
+
+print(#"""
+     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |
+"""#)
