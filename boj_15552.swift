b35c89a780c0d062576886d4112964de57095f55
chuncheonian <ehddud2468@gmail.com>
2022-04-27 22:04:16 +0900

    boj 15552 - 빠른 A+B : 구현

---
+++ boj_15552.swift
@@ -0,0 +1,42 @@
+// 2022.04.27
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/15552
+
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() }
+        if now == 45{ isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let fIO = FileIO()
+for _ in 1...fIO.readInt() {
+    print(fIO.readInt() + fIO.readInt())
+}
