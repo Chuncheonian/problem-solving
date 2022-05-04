e91a47485acf0b3d7fb41d9561da1c14a694f6d9
chuncheonian <ehddud2468@gmail.com>
2022-05-04 13:01:15 +0900

    boj 25083 - 새싹 : 구현

---
+++ boj_25083.swift
@@ -0,0 +1,12 @@
+// 2022.05.04
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/25083
+
+print(#"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      | 
+"""#)
