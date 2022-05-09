82be0d4f27738203dfcbfc77f138f606e968f02b
chuncheonian <ehddud2468@gmail.com>
2022-05-09 13:48:31 +0900

    boj 10866 - 덱 : 덱

---
+++ boj_10866.swift
@@ -0,0 +1,76 @@
+// 2022.05.09
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://www.acmicpc.net/problem/10866
+
+struct Deque<T> {
+    private var enqueueStack: [T]
+    private var dequeueStack: [T] = []
+
+    var count: Int {
+        return enqueueStack.count + dequeueStack.count
+    }
+
+    var isEmpty: Bool {
+        return enqueueStack.isEmpty && dequeueStack.isEmpty
+    }
+
+    var front: T? {
+        return dequeueStack.isEmpty ? enqueueStack.first : dequeueStack.last
+    }
+
+    var back: T? {
+        return enqueueStack.isEmpty ? dequeueStack.first : enqueueStack.last
+    }
+
+    init(_ queue: [T] = []) {
+        self.enqueueStack = queue
+    }
+
+    mutating func enqueue(_ element: T) {
+        enqueueStack.append(element)
+    }
+
+    mutating func enqueueFront(_ element: T) {
+        dequeueStack.append(element)
+    }
+
+    mutating func dequeue() -> T? {
+        if dequeueStack.isEmpty {
+            dequeueStack = enqueueStack.reversed()
+            enqueueStack.removeAll()
+        }
+        return dequeueStack.popLast()
+    }
+
+    mutating func dequeueFront() -> T? {
+        var value: T?
+        if enqueueStack.isEmpty {
+            dequeueStack.reverse()
+            value = dequeueStack.popLast()
+            dequeueStack.reverse()
+        } else {
+            value = enqueueStack.popLast()
+        }
+        return value
+    }   
+
+    mutating func removeAll() {
+        enqueueStack.removeAll()
+        dequeueStack.removeAll()
+    }
+}
+
+var deque = Deque<Int>()
+
+for _ in 1...Int(readLine()!)! {
+    let c = readLine()!.split{$0==" "}
+
+    if c[0]=="push_front" { deque.enqueueFront(Int(c[1])!) }
+    if c[0]=="push_back" { deque.enqueue(Int(c[1])!) }
+    if c[0]=="pop_front" { print(deque.isEmpty ? -1 : deque.dequeue()!) }
+    if c[0]=="pop_back" { print(deque.isEmpty ? -1 : deque.dequeueFront()!) }
+    if c[0]=="size" { print(deque.count) }
+    if c[0]=="empty" { print(deque.isEmpty ? 1 : 0) }
+    if c[0]=="front" { print(deque.isEmpty ? -1 : deque.front!) }
+    if c[0]=="back" { print(deque.isEmpty ? -1 : deque.back!) }
+}
