6d6fd68e48b00449e31348c46589eaca9cb93247
chuncheonian <ehddud2468@gmail.com>
2022-05-19 00:19:22 +0900

    programmers 12925 - 문자열을 정수로 바꾸기 : 구현

---
+++ programmers_12925.swift
@@ -0,0 +1,11 @@
+// 2022.05.19
+// Dongyoung Kwon @Chuncheonian (ehddud2468@gmail.com)
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+func solution(_ s:String) -> Int {
+    return Int(s)!
+}
+
+// Testcase
+print(solution("1234"))  // > 1234
+print(solution("4321"))  // > 4321
