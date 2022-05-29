3cf82e396cc423f03af478c8c01760fb7ed6f3f7
chuncheonian <ehddud2468@gmail.com>
2022-05-29 14:46:10 +0900

    boj 10989 - 수 정렬하기 3 : 정렬

---
+++ boj_10989.swift
@@ -0,0 +1,46 @@
+// 2022.05.29
+// Dongyoung Kwon @Chuncheonian (chuncheonian@gmail.com)
+// https://www.acmicpc.net/problem/10989
+
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)]
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() }
+        if now == 45{ isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let fIO = FileIO()
+var arr: [Int] = []
+
+for _ in 1...fIO.readInt() {
+    arr.append(fIO.readInt())
+}
+
+print(arr.sorted().map{ String($0) }.joined(separator: "\n"))
